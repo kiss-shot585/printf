b8fa58c0f09c74a1b1750cbc4b589a02dff230e5
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-19 04:41:00 +0100

    print

--- _printf.c
+++ _printf.c
@@ -1,52 +1,32 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <stddef.h>
 /**
- * _printf - recreates the printf function
- * @format: string with format specifier
- * Return: number of characters printed
+ * _printf - print a char or a string
+ * @format: It's a character string
+ * Return: the number of character the function is printing
  */
 int _printf(const char *format, ...)
 {
-	if (format != NULL)
-	{
-		int count = 0, i;
-		int (*m)(va_list);
-		va_list args;
+	st_fmt st_format[] = {
+		{"c", func_char},
+		{"s", func_string},
+		{"d", func_digit},
+		{"i", func_digit},
+		{"%", func_percent},
+		{"b", func_binary_convert},
+		{"u", func_unsig_int},
+		{"o", func_octal_convert},
+		{"x", func_hex_Lowcase_convert},
+		{"X", func_hex_Upcase_convert},
+		{"S", func_stringUppercase},
+		{"r", func_revstr},
+		{"R", func_rot13},
+		{NULL, NULL}};
 
-		va_start(args, format);
-		i = 0;
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-		while (format != NULL && format[i] != '\0')
-		{
-			if (format[i] == '%')
-			{
-				if (format[i + 1] == '%')
-				{
-					count += _putchar(format[i]);
-					i += 2;
-				}
-				else
-				{
-					m = get_func(format[i + 1]);
-					if (m)
-						count += m(args);
-					else
-						count = _putchar(format[i]) + _putchar(format[i + 1]);
-					i += 2;
-				}
-			}
-			else
-			{
-				count += _putchar(format[i]);
-				i++;
-			}
-		}
-		va_end(args);
-		return (count);
-	}
-	return (-1);
+	va_list list;
+	int count = 0;
+
+	va_start(list, format);
+	count =	get_match_func(format, list, st_format);
+	va_end(list);
+	return (count);
 }
