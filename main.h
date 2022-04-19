8e213bb6f6a8be5eeba4045a4ebf60b1d71e659d
kiss-shot585 <74220323+kiss-shot585@users.noreply.github.com>
2022-04-19 04:39:20 +0100

    main

--- main.h
+++ main.h
@@ -1,26 +1,39 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+
+
 /**
- * struct specifier - struct specifier
- * @valid: the valid character.
- * @f: the functions associated.
- *
+ * struct format - the struct by name stfmt
+ * @fmt: index by function
+ * @func: pointer to function - funtion print
  */
-typedef struct specifier
+typedef struct st_fmt
 {
-	char *valid;
-	int (*f)(va_list);
-} spec;
+	char *fmt;
+	int (*func)(va_list list);
+} st_fmt;
+
 int _printf(const char *format, ...);
-int print_c(va_list args);
-int print_s(va_list args);
-int print_d(va_list args);
-int print_i(va_list args);
 int _putchar(char c);
-int print_percent(va_list args);
-int (*get_func(char x))(va_list args);
+int func_string(va_list);
+int func_char(va_list);
+int func_percent(va_list);
+int get_match_func(const char *, va_list, st_fmt st_format[]);
+int func_digit(va_list);
+int func_binary_convert(va_list);
+int func_octal_convert(va_list);
+int func_hex_Upcase_convert(va_list);
+int func_hex_Lowcase_convert(va_list);
+int binary_oct_hex_convert(unsigned int, int, int);
+int func_unsig_int(va_list list);
+int func_stringUppercase(va_list list);
+int func_revstr(va_list list);
+int func_rot13(va_list);
 
 #endif
